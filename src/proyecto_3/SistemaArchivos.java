6dede16b5233665d7e699c7c01b9ded918e4b5d2
Heiner Salvatierra Ramírez <Heiner007@outlook.es>
2019-11-08 14:08:32 -0600

    Comandos format (falta ajustar el tamaño del archivo) y adduser(solo root)

--- src/proyecto_3/SistemaArchivos.java
+++ src/proyecto_3/SistemaArchivos.java
@@ -5,7 +5,10 @@
  */
 package proyecto_3;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -61,6 +64,9 @@ public class SistemaArchivos {
                 System.out.print("Usuario: ");
                 lineaActual = entradaComandos.nextLine();
                 historialComandos += lineaActual + "\n";
+                if(lineaActual.equals("exit")){
+                    break;
+                }
                 manejadorComandos(lineaActual);
             }
         }
@@ -72,25 +78,26 @@ public class SistemaArchivos {
      * información del archivo (comando "format").
      */
     private void cargarSistemaArchivos(){
-        File archivo = new File(nombreDisco);
-        try {
-            if(!archivo.exists()){
-                archivo.createNewFile();
-            }
-        } catch (IOException ex) {
-            Logger.getLogger(SistemaArchivos.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        existeDisco();
+        
         System.out.println("Sistema de archivos cargado");
         sistemaCargado = true;
+    }
+    
+    private void existeDisco(){
+        File archivo = new File(nombreDisco);
+        if(!archivo.exists()){
+            comandoFormat();
+        }
     }
     
     private void manejadorComandos(String comando){
         switch (comando){
             case "format":
-                // llamado al método
+                comandoFormat();
                 break;
             case "useradd":
-                // llamado al método
+                comandoUserAdd(false);
                 break;
             case "groupadd":
                 // llamado al método
@@ -160,6 +167,147 @@ public class SistemaArchivos {
                 break;
             default:
                 break;
+        }
+    }
+    
+    private void comandoFormat(){
+        String tamanioDiscoTemp;
+        do{
+            System.out.print("Ingrese el tamaño del disco en MB: ");
+            tamanioDiscoTemp = entradaComandos.nextLine();
+        }while(!esNumero(tamanioDiscoTemp));
+        
+        tamanioDisco = Integer.valueOf(tamanioDiscoTemp);
+        comandoUserAdd(true);
+        crearSistemaArchivos();
+        System.out.println("Formato creado");
+    }
+    
+    private void comandoUserAdd(Boolean root){
+        String nombreUsuario, contrasenia, contraseniaTemp;
+        if(root){
+            nombreUsuario = "root";
+        }else{
+            System.out.print("Ingrese el nombre completo");
+            nombreUsuario = entradaComandos.nextLine();
+        }
+        do{
+            System.out.print("Ingrese la contraseña del "+nombreUsuario+": ");
+            contrasenia = entradaComandos.nextLine();
+            if(!contrasenia.isEmpty()){
+                System.out.print("Confirme la contraseña: ");
+                contraseniaTemp = entradaComandos.nextLine();
+                if(!contrasenia.equals(contraseniaTemp)){
+                    System.out.println("Las contraseñas deben ser iguales");
+                }
+            }else{
+                contraseniaTemp = null;
+                System.out.println("Ingrese un valor válido.");
+            }
+        }while(!contrasenia.equals(contraseniaTemp));
+        
+        if(root){
+            usuarioActual = new Usuario(0, nombreUsuario, contrasenia);
+        }else{
+            
+        }
+    }
+    
+    private String generarContenido(){
+        int cantidadBloques = (tamanioDisco * 1024) / 512;
+        int tamanioBloques = 512;
+        String cBloquesLibres = "1";
+        for(int i = 1; i < cantidadBloques; i++){
+            cBloquesLibres += ",0";
+        }
+        String bloquesDisco =
+                "[B]\n" +
+                "[BS]\n" +
+                "-1\n" +
+                "[/BS]\n" +
+                "[I]\n" +
+                "[Id]\n" +
+                "0\n" +
+                "[/Id]\n" +
+                "[1]\n" +
+                tamanioDisco+"\n" +
+                "[/1]\n" +
+                "[2]\n" +
+                cantidadBloques+"\n" +
+                tamanioBloques+"\n" +
+                "[/2]\n" +
+                "[3]\n" +
+                cBloquesLibres+"\n" +
+                "[/3]\n" +
+                "[4]\n" +
+                "[U]\n" +
+                "[Id]\n" +
+                "0\n" +
+                "[/Id]\n" +
+                "[N]\n" +
+                "root\n" +
+                "[/N]\n" +
+                "[Con]\n" +
+                usuarioActual.contasenia+"\n" +
+                "[/Con]\n" +
+                "[/U]\n" +
+                "[/4]\n" +
+                "[5]\n" +
+                "[GU]\n" +
+                "[Id]\n" +
+                "0\n" +
+                "[/Id]\n" +
+                "[N]\n" +
+                "GrupoRoot\n" +
+                "[/N]\n" +
+                "[U]\n" +
+                "[Id]\n" +
+                "0\n" +
+                "[/Id]\n" +
+                "[/U]\n" +
+                "[/GU]\n" +
+                "[/5]\n" +
+                "[/I]\n" +
+                "[/B]\n";
+        for(int i = 1; i < cantidadBloques; i++){
+            bloquesDisco +=
+                    "[B]\n" +
+                    "[BS]\n" +
+                    "-1\n" +
+                    "[/BS]\n" +
+                    "[I]\n" +
+                    "[Id]\n" +
+                    i+"\n" +
+                    "[/Id]\n" +
+                    "[/I]\n" +
+                    "[/B]\n";
+        }
+        return bloquesDisco;
+    }
+    
+    private void crearSistemaArchivos(){
+        File archivo = new File(nombreDisco);
+        FileWriter fw;
+        BufferedWriter bw;
+        try {
+            if(!archivo.exists()){
+                archivo.createNewFile();
+            }fw = new FileWriter(archivo);
+            bw = new BufferedWriter(fw);
+            bw.write(generarContenido());
+            bw.close();
+        } catch (IOException ex) {
+            Logger.getLogger(SistemaArchivos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+    private Boolean esNumero(String numeroTemp){
+        try{
+            int numero = Integer.valueOf(numeroTemp);
+            return numero > 0;
+        }catch(NumberFormatException e){
+            return false;
         }
     }
 }
